0613cb2295ec0e33636cc8ca9a8643644e6be207
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2024-10-07 22:44:12 +0100

    Update subroutines.sh

--- app/subroutines.sh
+++ app/subroutines.sh
@@ -1,6 +1,8 @@
 # Miscellaneous functions are defined here
 
-
-# Scan local wifi networks
-log_info "Scanning wifi:
-  `iwlist  $WLAN_INTERFACE s | grep 'Cell\|Quality\|ESSID\|IEEE'"
+function setup_wifi() {
+  # Scan local wifi networks
+  log_info "Scanning wifi:
+    `iwlist  $WLAN_INTERFACE s | grep 'Cell\|Quality\|ESSID\|IEEE'`"
+    
+}
