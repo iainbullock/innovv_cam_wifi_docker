83ed858121cab3e9fe080e0f8c6c41a9b7853a54
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2024-10-07 23:13:48 +0100

    Update subroutines.sh

--- app/subroutines.sh
+++ app/subroutines.sh
@@ -4,5 +4,7 @@ function setup_wifi() {
   # Scan local wifi networks
   log_info "Scanning wifi:
   `iwlist $WLAN_INTERFACE s | grep 'Cell\|Quality\|ESSID\|IEEE'`"
-    
+
+  # 
+  echo $WLAN_PASSWORD | wpa_passphrase $WLAN_NAME > /config/wpa_supplicant.conf
 }
