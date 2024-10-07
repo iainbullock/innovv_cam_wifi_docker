4840307f5f04afd55a1e4380a04e7d74d54ac086
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2024-10-07 19:59:15 +0100

    Update libproduct.sh

--- libproduct.sh
+++ libproduct.sh
@@ -1,6 +1,11 @@
 function validateEnvVars() {
   exitOnError=0
 
+  if [ "$DEBUG" != 'true' ] && [ "$DEBUG" != 'false' ]; then
+    log_fatal "Fatal; DEBUG:$DEBUG is not compliant, please check this setting"
+    exitOnError=1
+  fi
+
   if [ $exitOnError -eq 0 ]; then
     :
   else
