9d44205361a3dfbff5b2072dd7b7ed8908e24f36
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2024-10-07 22:26:38 +0100

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -5,6 +5,9 @@ echo "[$(date +%H:%M:%S)] Starting..."
 echo "Loading /app/env.sh"
 . /app/env.sh
 
+# View Interfaces
+iwconfig 2>/dev/null | cat
+
 # Start main program loop
 log_info "Entering main loop..."
 while :; do
